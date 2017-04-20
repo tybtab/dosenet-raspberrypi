9c0a4ed3ba4fb951891e4eb3f2279a622caa4331
tybtab <tybtab@berkeley.edu>
2017-04-20 13:25:03 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -4,16 +4,6 @@ import ast
 print('Running Test Script')
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=5)
 
-def toHex(s):
-    lst = []
-    for ch in s:
-        hv = hex(ord(ch)).replace('0x', '')
-        if len(hv) == 1:
-            hv = '0'+hv
-        lst.append(hv)
-    
-    return reduce(lambda x,y:x+y, lst)
-
 def toStr(s):
     return s and chr(int(s[:2], base=10)) + toStr(s[2:]) or ''
 
