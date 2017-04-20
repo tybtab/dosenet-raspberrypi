8be3054bc68154b5829bd351a980c8e62d7a265c
tybtab <tybtab@berkeley.edu>
2017-04-20 12:17:22 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -6,4 +6,4 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1.5)
 while True: 
     print('in loop')
     recv = port.readline()
-    port.write("\r\nYou sent:" + repr(rcv))
+    port.write("\r\nYou sent:" + repr(recv))
