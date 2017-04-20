492b8b601dbf27369c8a2a1b6fa4e8043711f135
tybtab <tybtab@berkeley.edu>
2017-04-20 12:14:53 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -1,7 +1,7 @@
 import serial
 
 print('Running Test Script')
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1.5)
 
 while True: 
     print('in loop')
