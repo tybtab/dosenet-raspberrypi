5deb3ec22ca721e9e269b9fab0e4b0d703c92bdc
tybtab <tybtab@berkeley.edu>
2017-04-21 11:43:51 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -1,11 +1,14 @@
 import serial
 import ast
+import binascii
 
 print('Running Test Script')
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=5)
 while True: 
     print('next')
     recv = port.read(32)
-    print(repr(recv))
+    x = binascii.unhexlify(recv).decode('utf8')
+    #print(unpack(recv))
     #print(repr(recv)[3:7].decode('hex'))
-    print(repr(recv).decode("utf-8"))
+    #print(repr(recv).decode("utf-8"))
+    print(x)
