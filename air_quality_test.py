783c2f2f858a514c5f983a6493887b910d210257
tybtab <tybtab@berkeley.edu>
2017-04-21 12:00:50 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -6,7 +6,7 @@ print('Running Test Script')
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=5)
 while True: 
     print('next')
-    recv = port.read(33)
+    recv = port.read(32)
     print(type(recv))
     x = binascii.hexlify(recv).decode('utf8')
     print(x)
