c78c45faafc8da012d465719ad98245a452a6a83
tybtab <tybtab@berkeley.edu>
2017-04-24 11:50:12 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -8,6 +8,7 @@ port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=5)
 while True: 
     print('next')
     recv = port.read(10)
+    print(recv)
     if(recv[0]==0xaa and recv[1]==0xc0):
         print('True')
     else:
