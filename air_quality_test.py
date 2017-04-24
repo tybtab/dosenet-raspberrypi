89bc023b4937b88ca93e9e9bdb4b8a02b0952529
tybtab <tybtab@berkeley.edu>
2017-04-24 11:48:53 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -1,4 +1,4 @@
-'''
+
 import serial
 import ast
 import binascii
@@ -7,18 +7,23 @@ print('Running Test Script')
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=5)
 while True: 
     print('next')
-    recv = port.read(32)
-    print(recv)
-    print(binascii.b2a_uu(recv))
-    print(binascii.b2a_uu(recv[5]), binascii.b2a_uu(recv[6]))
+    recv = port.read(10)
+    if(retstr[0]==0xaa and retstr[1]==0xc0):
+        print('True')
+    else:
+        print('False')
+    #print(recv)
+    #print(binascii.b2a_uu(recv))
+    #print(binascii.b2a_uu(recv[5]), binascii.b2a_uu(recv[6]))
     #x = binascii.b2a_uu(recv)
     #print(x)
     #print(recv.decode('hex'))
     #print(repr(recv))
     #x = recv.encode('hex')
     #print(x.decode('ascii'))
-'''
 
+
+'''
 #http://www.instructables.com/id/Using-Pm25-Sensor-With-Raspberry-Pi/
 import serial
 import time
@@ -47,6 +52,6 @@ while True:
                 pm25=int(retstr[2])+int(retstr[3])*256
                 pm10=int(retstr[4])+int(retstr[5])*256
                 print ("pm2.5:%.1f pm10 %.1f"%(pm25/10.0,pm10/10.0))
-
+'''
 
     
