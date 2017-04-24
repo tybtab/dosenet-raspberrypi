99bdeb15dfe658d6279c7d7d117d8de691fa8fab
tybtab <tybtab@berkeley.edu>
2017-04-24 13:18:58 -0700

    Update air_quality_test.py

--- air_quality_test.py
+++ air_quality_test.py
@@ -13,39 +13,39 @@ while True:
     #Check sum with last byte of list 
     sumation = sum(buffer[0:31])
 
-    if sumation == sum(buffer[30:32]):
-        print('Sum check complete')
-        buf = buffer[1:32]
+    #if sumation == sum(buffer[30:32]):
+    #print('Sum check complete')
+    buf = buffer[1:32]
 
-        # Get concentrations ug/m3
-        PM01Val=((buf[3]<<8) + buf[4])
-        PM25Val=((buf[5]<<8) + buf[6])
-        PM10Val=((buf[7]<<8) + buf[8])
+    # Get concentrations ug/m3
+    PM01Val=((buf[3]<<8) + buf[4])
+    PM25Val=((buf[5]<<8) + buf[6])
+    PM10Val=((buf[7]<<8) + buf[8])
 
-        # Get number of particles in 0.1 L of air above specific diameters
+    # Get number of particles in 0.1 L of air above specific diameters
 
 
-        P3  =((buf[15]<<8) + buf[16])
-        P5  =((buf[17]<<8) + buf[18])
-        P10 =((buf[19]<<8) + buf[20])
-        P25 =((buf[21]<<8) + buf[22])
-        P50 =((buf[23]<<8) + buf[24])
-        P100=((buf[25]<<8) + buf[26])
+    P3  =((buf[15]<<8) + buf[16])
+    P5  =((buf[17]<<8) + buf[18])
+    P10 =((buf[19]<<8) + buf[20])
+    P25 =((buf[21]<<8) + buf[22])
+    P50 =((buf[23]<<8) + buf[24])
+    P100=((buf[25]<<8) + buf[26])
 
-        # Print Concentrations [ug/m3]
-        print('\nConcentration of Particulate Matter [ug/m3]\n')
-        print('PM 1.0 = ' + repr(PM01Val) +' ug/m3')
-        print('PM 2.5 = ' + repr(PM25Val) +' ug/m3')
-        print('PM 10  = ' + repr(PM10Val) +' ug/m3\n')
+    # Print Concentrations [ug/m3]
+    print('\nConcentration of Particulate Matter [ug/m3]\n')
+    print('PM 1.0 = ' + repr(PM01Val) +' ug/m3')
+    print('PM 2.5 = ' + repr(PM25Val) +' ug/m3')
+    print('PM 10  = ' + repr(PM10Val) +' ug/m3\n')
 
-        # Print number of particles in 0.1 L of air over specific diamaters
-        print('Number of particles in 0.1 L of air with specific diameter\n')
-        print('#Particles, diameter over 0.3 um = ' + repr(P3))
-        print('#Particles, diameter over 0.5 um = ' + repr(P5))
-        print('#Particles, diameter over 1.0 um = ' + repr(P10))
-        print('#Particles, diameter over 2.5 um = ' + repr(P25))
-        print('#Particles, diameter over 5.0 um = ' + repr(P50))
-        print('#Particles, diameter over 10  um = ' + repr(P100))
+    # Print number of particles in 0.1 L of air over specific diamaters
+    print('Number of particles in 0.1 L of air with specific diameter\n')
+    print('#Particles, diameter over 0.3 um = ' + repr(P3))
+    print('#Particles, diameter over 0.5 um = ' + repr(P5))
+    print('#Particles, diameter over 1.0 um = ' + repr(P10))
+    print('#Particles, diameter over 2.5 um = ' + repr(P25))
+    print('#Particles, diameter over 5.0 um = ' + repr(P50))
+    print('#Particles, diameter over 10  um = ' + repr(P100))
 
-    else:
-        print('Check Sum Failed')
+    #else:
+        #print('Check Sum Failed')
